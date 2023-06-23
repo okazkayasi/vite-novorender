1c58f2c845608022e2417d88ed342962bf1f6f33
Oguz Kazkayasi <oguz.kazkayasi@cleevio.com>
2023-06-24 00:53:28 +0300

    fix: not required

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -56,7 +56,6 @@ export const Search = (props: ISearchComponent) => {
             name="searchParam"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="Chair"
-            required
           />
           <button
             type="submit"
